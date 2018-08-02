50bed8264653fbd1b9fe5532bf5accd01451e8f2
Karan Prasad <karan.prasad95@gmail.com>
2018-08-02 16:30:22 +0530

    Update p.c

--- a30/p.c
+++ a30/p.c
@@ -1,15 +1,20 @@
 #define ANSI_ANY "\x1b[46m"
+#define ANSI_ANY "\x1b[31m"
 #define ANSI_ANY2 "\x1b[0m"
 #include<stdio.h>
 
-void rest(char b[], char rep){
+int rest(char b[], char rep, int col){
 char e[]="";
 int count = 0,i=0,j=0;
-int ce;
+col=0;
 do{//do1
 count++;
 }while(b[i++]!=e[0]);//do1
 int loop=count;
+//count
+i=0;
+do{if(b[i]==rep){col++;}}while(b[i++]!=e[0]);
+//main
 i=0;
 j=0;
 do{//do2
@@ -18,16 +23,21 @@ j=i;
 do{if(b[i]!=rep){b[j]=b[i];j++;}}while(i++<loop);
 }//if
 }while(i++<loop);//do2
+return col; 
 }// main
 
 int main(){
 char a[3000];
 char replace;
+int cxx;
 printf(ANSI_ANY"Enter the paragraph you want to edit ::: \n"ANSI_ANY2);
 gets(a);
 printf(ANSI_ANY"Enter the charater you want to eliminate ::: \n"ANSI_ANY2);
 scanf("%c",&replace);
+int jack = resr(a,replace,cxx);
+rest(a,replace,cxx);
+printf(ANSI_ANY"Number of alphabets to be eliminated :::\n"ANSI_ANY2);
+printf(ANSI_ANY3"%d\n"ANSI_ANY2,jack);
 printf(ANSI_ANY"Edited parapraph ::: \n"ANSI_ANY2);
-rest(a,replace);
 printf("%s\n",a);
 }
